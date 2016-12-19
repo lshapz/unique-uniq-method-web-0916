45dcd941f9733497e6b1cf6ab6100a200893285b
lshapz <laurashapiro01@gmail.com>
2016-12-19 15:07:58 -0500

    Done.

--- lib/unique_uniq_method.rb
+++ lib/unique_uniq_method.rb
@@ -1,5 +1,15 @@
 class Array
   def unique_uniq
     # code goes here
+    array = self
+    new_array = []
+    array.each do |item| 
+      if !new_array.include?(item)
+        new_array.push(item)
+      end
+    end
+    new_array
+
+
   end
 end
